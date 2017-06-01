ae0a3d0ac40c2defcc62e735147f9d76c1bcd8a2
koriege <koriege@gmail.com>
2017-06-01 17:15:01 +0200

    setup fixed

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 dir=$(cd $(dirname $0) && pwd)
 for i in $(ls -d $dir/*/); do
-	ln -s $i $GALAXY_ROOT/config/plugins/webhooks/$(basename $i)
+	ln -sfn $i $GALAXY_ROOT/config/plugins/webhooks/$(basename $i)
 done
 
 sed -i -r 's/^\s*#+(\s*webhooks_dir.+)/\1/' $GALAXY_ROOT/config/galaxy.ini
