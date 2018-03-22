d78fda64c881fa13b094cce3623d92cb15131de0
koriege <koriege@gmail.com>
2018-03-22 18:03:15 +0100

    production of de.stair tours and final step detection

--- switchtour/helper/__init__.py
+++ switchtour/helper/__init__.py
@@ -7,9 +7,12 @@ def main(trans, webhook):
     error = ''
     try:
         from bioblend import galaxy
-        import cStringIO
-        import json
+        # import cStringIO
+        # import json
+        # import re
         from galaxy.webapps.galaxy.api.workflows import WorkflowsAPIController
+        from galaxy.managers.users import UserManager
+        # from galaxy.webapps.galaxy.api.tours import ToursController
     except ImportError as e:
         error = str(e)
         print(error)
@@ -19,16 +22,16 @@ def main(trans, webhook):
     lasttool = ''
     commands = ''
     try:
-        apikey = trans.user.api_keys[0].key
+        print("trans========================")
+        # print(dir(trans.user))
+        # trans.sa_session.query(trans.app.model.APIKeys)
+        # trans.sa_session.query(trans.app.model.Histories)
+        
+        #apikey = trans.user.api_keys[0].key
+        apikey = UserManager(trans.app).get_or_create_valid_api_key(trans.user).key
         gi = galaxy.GalaxyInstance(url='http://127.0.0.1:8080', key=apikey)
         for i in gi.workflows.get_workflows():
             gi.workflows.delete_workflow(i['id'])
-
-        print("trans========================")
-        print(dir(trans))
-        # trans.workflow_building_mode = True
-        # trans.sa_session.query(trans.app.model.APIKeys)
-        # trans.sa_session.query(trans.app.model.Histories)
 
         print("history========================")
         has_hastories = False
@@ -49,7 +52,7 @@ def main(trans, webhook):
             commands = commands + "\n" + i.creating_job.get_command_line() if commands else i.creating_job.get_command_line()
             has_hastories = True
 
-        print("workflow========================")
+        print "workflow========================"
 
         WorkflowsAPIController(trans.app).create(trans, {'from_history_id': history_id, 'workflow_name': 'tmp', 'job_ids': job_ids, 'dataset_ids': [], 'dataset_collection_ids': []})
         # w = json.loads(WorkflowsAPIController(trans.app).create(trans, {'from_history_id': history_id, 'workflow_name': 'tmp', 'job_ids': job_ids, 'dataset_ids': [], 'dataset_collection_ids': []}))
@@ -61,6 +64,12 @@ def main(trans, webhook):
             # download = cStringIO.StringIO()
             # gi.histories.download_history(history_id, jeha_id, download, chunk_size=4096)
             # workflow = download.getvalue()
+
+        # print "tours========================"
+        # for i in json.loads(ToursController(trans.app).index(trans)):
+        #     m = re.search('tour_(\d+)',i['id'])
+        #     if m:
+        #         print m.group(1)
 
         if has_hastories and lasttool == 'nuke':
             for i in gi.histories.get_histories():
